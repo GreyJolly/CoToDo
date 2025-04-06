9dc8640543be1078f2257d50630b5f7ff340eee6
GreyJolly <vortex.minecraft@libero.it>
2025-04-06 18:21:32 +0200

    Improved Filepath handling

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
-      .then(registration => console.log('SW registered'))
-      .catch(err => console.log('SW registration failed: ', err));
-  });
-}
+	window.addEventListener('load', () => {
+	  navigator.serviceWorker.register('./sw.js', { scope: './' })
+		.then(registration => console.log('SW registered'))
+		.catch(err => console.log('SW registration failed: ', err));
+	});
+  }
