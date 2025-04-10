92171d4066dd7305a3d0b93c3315279bc7f359eb
GreyJolly <vortex.minecraft@libero.it>
2025-04-10 10:41:40 +0200

    Updated Service Worker Cache

--- sw.js
+++ sw.js
@@ -1,22 +1,28 @@
-const CACHE_NAME = 'cotodo-cache-v2';
+const CACHE_NAME = 'cotodo-cache-v3';
 
 const urlsToCache = [
-  './',
-  './stylesheets/styles.css',
-  './scripts/index.js',
-  './images/icon-512.png',
-  './images/icon-192.png'
+	'./',
+	'./index.html',
+	'./profile.html',
+	'./calendar.html',
+	'./stylesheets/index.css',
+	'./stylesheets/buttons.css',
+	'./stylesheets/profile.css',
+	'./stylesheets/calendar.css',
+	'./scripts/index.js',
+	'./images/icon-512.png',
+	'./images/icon-192.png'
 ];
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then((cache) => cache.addAll(urlsToCache))
+	);
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+	event.respondWith(
+		caches.match(event.request)
+			.then((response) => response || fetch(event.request))
+	);
 });
